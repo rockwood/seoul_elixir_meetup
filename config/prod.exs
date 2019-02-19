b88aa63022d294f6aa8d9dfb5bb4a807aec856e0
Kevin Rockwood <kevin@rockwood.me>
2019-02-19 09:02:30 +0900

    Setup for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,14 @@ use Mix.Config
 # before starting your production server.
 config :sem, SemWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: System.get_env("HOSTNAME"), port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
+config :sem, Sem.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -65,7 +71,3 @@ config :logger, level: :info
 #
 # Note you can't rely on `System.get_env/1` when using releases.
 # See the releases documentation accordingly.
-
-# Finally import the config/prod.secret.exs which should be versioned
-# separately.
-import_config "prod.secret.exs"
